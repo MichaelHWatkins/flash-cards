21955011ec1aa34c436afa5a0e3f2a3381cf3368
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-10-19 11:46:51 -0700

    Made the flashcards template...
    Co-authored-by: Hannah Young <hannah@corgibyte.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+## JavaScript Flash Cards
+
+By Hannah Young and Mike Watkins
+
+A website of flashcards to learn JavaScript definitions.
+
+### Technologies Used
+* HTML
+* CSS (including Bootstrap)
+* JavaScript (including JQuery)
+
+### Description
+
+To follow
+
+### Setup
+1. Download source folder
+2. Open `index.html` in your browser
---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#term-1").click(function() {
+    $("#def-1").fadeToggle();
+  });
+
+  $("#term-2").click(function() {
+    $("#def-2").fadeToggle();
+  });
+
+  $("#term-3").click(function() {
+    $("#def-3").fadeToggle();
+  });
+
+  $("#term-4").click(function() {
+    $("#def-4").fadeToggle();
+  });
+
+  $("#term-5").click(function() {
+    $("#def-5").fadeToggle();
+  });
+
+  $("#term-6").click(function() {
+    $("#def-6").fadeToggle();
+  });
+});
